0b5681a81e155950f48bed765b28ba809e3ef13e
Nurgul Elmali <101966469+nrglelml@users.noreply.github.com>
2023-05-09 10:35:07 +0300

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,12 +13,15 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+             $table->string('username')->unique();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->bigInteger('phone_number')->unique();
+            $table->timestamp('phone_number_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+
         });
     }
 
