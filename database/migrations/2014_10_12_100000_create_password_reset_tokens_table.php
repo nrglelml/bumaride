2c350ca02ce2b927c082ba47f27fe38e301383e8
Nurgul Elmali <101966469+nrglelml@users.noreply.github.com>
2023-05-09 11:56:50 +0300

    Update 2014_10_12_100000_create_password_reset_tokens_table.php

--- database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
+++ database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('phone_number')->primary();
+            $table->bigInteger('phone')->primary();
             $table->string('token');
             $table->timestamp('created_at')->nullable();
         });
